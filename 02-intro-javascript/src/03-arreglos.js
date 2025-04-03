7c7e468d15d391951d865870826999eb82cf0e12
Fabztech <fabztech@outlook.com>
2025-04-03 06:33:24 -0500

    Intro JS - Arrays

---
+++ 02-intro-javascript/src/03-arreglos.js
@@ -0,0 +1,49 @@
+
+
+const arreglo = new Array( 5 );
+arreglo.push(1);
+arreglo.push(2);
+
+const arreglo2 = [];
+arreglo2.push(1);
+arreglo2.push(2);
+arreglo2.push(3);
+arreglo2.push(4);
+
+
+const arreglo3 = new Array();
+arreglo3.push(1);
+arreglo3.push(2);
+
+const arregloTask = [ ...arreglo2, 5 ]
+
+const mapArreglo2Mult2 = arreglo2.map((n) => n*2)
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
+console.log(arregloTask);
+console.log(mapArreglo2Mult2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
