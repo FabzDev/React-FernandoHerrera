480887c9c0f1d6077fc9a4eeb9a2981316c21fa8
Fabztech <fabztech@outlook.com>
2025-04-05 14:24:05 -0500

    Intro JS - async/await

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,26 +1,16 @@
-const API_KEY = "bkUpWx7ebyORXlTH747bYSgrAmAw64BC";
-
-const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
-
-//Long Syntax
-// peticion.then( resp => {
-//     resp.json().then( jsonData => {
-//         const {data} = jsonData;
-//         const img = document.createElement('img');
-//         img.src = data.images.original.url;
-//         document.body.appendChild(img);
-//     })
-// })
-
-//Simple Sintax
-peticion
-.then((resp) => resp.json())
-.then((dataJson) => {
-    const {data} = dataJson;
-    const img = document.createElement('img');
+const getImage = async () => {
+  try {
+    const API_KEY = "bkUpWx7ebyORXlTH747bYSgrAmAw64BC";
+    const resp = await fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    );
+    const { data } = await resp.json();
+    const img = document.createElement("img");
     img.src = data.images.original.url;
     document.body.appendChild(img);
-})
-.catch(console.warn);
+  } catch (err) {
+    console.error("Manejor de error", err);
+  }
+};
 
-
+getImage();
