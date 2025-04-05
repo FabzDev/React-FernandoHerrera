477d2ddaefccd4e6623c3e5e9e9c22e225be6599
Fabztech <fabztech@outlook.com>
2025-04-05 14:33:29 -0500

    Intro JS - ternario // fin de la sección Intro JS

--- 02-intro-javascript/src/bases/11-async-await.js
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -1,17 +1,16 @@
 const getImage = async () => {
-    try {
-      const API_KEY = "bkUpWx7ebyORXlTH747bYSgrAmAw64BC";
-      const resp = await fetch(
-        `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
-      );
-      const { data } = await resp.json();
-      const img = document.createElement("img");
-      img.src = data.images.original.url;
-      document.body.appendChild(img);
-    } catch (err) {
-      console.error("Manejor de error", err);
-    }
-  };
-  
-  getImage();
-  
+  try {
+    const API_KEY = "bkUpWx7ebyORXlTH747bYSgrAmAw64BC";
+    const resp = await fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    );
+    const { data } = await resp.json();
+    const img = document.createElement("img");
+    img.src = data.images.original.url;
+    document.body.appendChild(img);
+  } catch (err) {
+    console.error("Manejor de error", err);
+  }
+};
+
+getImage();
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,16 +1,12 @@
-const getImage = async () => {
-  try {
-    const API_KEY = "bkUpWx7ebyORXlTH747bYSgrAmAw64BC";
-    const resp = await fetch(
-      `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
-    );
-    const { data } = await resp.json();
-    const img = document.createElement("img");
-    img.src = data.images.original.url;
-    document.body.appendChild(img);
-  } catch (err) {
-    console.error("Manejor de error", err);
-  }
-};
 
-getImage();
+
+const activo = true;
+
+// const mensaje = activo?'Activo':'Inactivo';
+
+// const mensaje = activo?'Activo':'null';
+const rafa = 'Rafa Orozco';
+
+const test = activo && rafa;
+
+console.log(test);
