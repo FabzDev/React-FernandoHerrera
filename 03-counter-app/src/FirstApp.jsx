bc298324aaca592ed4ccd8e5d5fc7d6b5f4f0cb8
Fabztech <fabztech@outlook.com>
2025-04-06 16:13:51 -0500

    React Basics - React components comunication - Props

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,3 +1,26 @@
-export const FirstApp = () => {
-    return <h1>First App</h1>
-}
+// import { Fragment } from "react";
+
+// export const FirstApp = () => {
+//   return (
+//     <Fragment>
+//       <h1>Fabio Escobar</h1>
+//       <h2>First App</h2>
+//     </Fragment>
+//   );
+// };
+
+
+const getNombre = (nombre) => nombre
+
+// export const FirstApp = (props) => {
+export const FirstApp = ({nombre, title}) => {
+
+  return (
+    <>
+      <h1>{getNombre(nombre)}</h1>
+      <h2>First App</h2>
+      {/* <h3>Title: {props.title}</h3> */}
+      <h3>Title: {title}</h3>
+    </>
+  );
+};
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
+import './styles.css';
 
-function App(){
-    return <h1>Hola Mundo!!</h1>
-}
 
 ReactDOM.createRoot( document.getElementById("root"))
 .render(
     <React.StrictMode>
-        <FirstApp />
+        <FirstApp nombre = "Fazdev" title="El Jaken 2"/>
     </React.StrictMode>    
 )
