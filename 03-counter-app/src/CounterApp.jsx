8f988c94968d255ba81b00d51af50a70c14b5fec
Fabztech <fabztech@outlook.com>
2025-04-06 19:06:36 -0500

    React Basics - CounterApp task practice

---
+++ 03-counter-app/src/CounterApp.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const CounterApp = ({value=0}) => {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <h2>{value}</h2>
+      <button>click</button>
+    </>
+  );
+};
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client"
 import { HelloWorldApp } from "./HelloWorldApp";
 import { FirstApp } from "./FirstApp";
 import './styles.css';
+import { CounterApp } from "./CounterApp";
 
 
 ReactDOM.createRoot( document.getElementById("root"))
 .render(
     <React.StrictMode>
-        <FirstApp nombre="Inuyasha!" title="Seshomaru ataca de nuevo..."/>
+        {/* <FirstApp nombre="Inuyasha!" title="Seshomaru ataca de nuevo..."/> */}
+        <CounterApp value={10} />
     </React.StrictMode>    
 )
