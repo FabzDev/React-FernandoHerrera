5449b54b713e0e8b64cd6cf840a1d9004de62471
Fabztech <fabztech@outlook.com>
2025-04-12 18:01:01 -0500

    Testing Section - Done

--- 03-counter-app/tests/CounterApp.test.jsx
+++ 03-counter-app/tests/CounterApp.test.jsx
@@ -1,10 +1,7 @@
-// Test: debe hacer match con el snapshoot
-
-import { screen } from "@testing-library/dom";
+import { fireEvent, getByRole, screen } from "@testing-library/dom";
 import { CounterApp } from "../src/CounterApp";
 import { render } from "@testing-library/react";
 
-// Test: debe mostrar el valor inicial de 100 <CounterApp value={100}/>
 describe("Tests on <CounterApp/> Component", () => {
   const initValueExp = 100;
 
@@ -15,9 +12,32 @@ describe("Tests on <CounterApp/> Component", () => {
 
   test("should render initial value", () => {
     render(<CounterApp initValue={initValueExp} />);
-    expect(screen.getByRole('heading',{level:2}).innerHTML)
-    .toBe(initValueExp.toString())
+    expect(screen.getByRole("heading", { level: 2 }).innerHTML).toBe(
+      initValueExp.toString()
+    );
     // expect(screen.getByText(initValueExp)).toBeTruthy();
-});
+  });
 
+  //Learning: fireEvent.click(_element_)
+  test("should increment 'counter' after clicking button +1", () => {
+    render(<CounterApp initValue={initValueExp} />);
+    fireEvent.click( screen.getByRole('button', {name:'+1'}));
+    expect(screen.getByText(initValueExp+1).innerHTML).toBe((initValueExp+1)+"");
+  });
+
+  test("should decrease 'counter' after clicking button -1", () => {
+    render(<CounterApp initValue={initValueExp} />);
+    fireEvent.click( screen.getByRole('button', {name:'-1'}) );
+    expect(screen.getByText(initValueExp-1)).toBeTruthy();
+  });
+
+  test("should reset 'counter' after clicking reset button", () => {
+    render(<CounterApp initValue={initValueExp} />);
+    fireEvent.click( screen.getByRole('button', {name:'+1'}));
+    fireEvent.click( screen.getByRole('button', {name:'+1'}));
+    fireEvent.click( screen.getByRole('button', {name:'+1'}));
+    fireEvent.click( screen.getByRole('button', {name:'-1'}) );
+    fireEvent.click( screen.getByRole('button', {name:'Reset'}) );
+    expect(screen.getByText(initValueExp)).toBeTruthy();
+  });
 });
