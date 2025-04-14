6e110084d3d816899e936b83acedc3314476636e
Fabztech <fabztech@outlook.com>
2025-04-14 00:45:39 -0500

    GifExpertApp - Added New Category (refactor)

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 // bkUpWx7ebyORXlTH747bYSgrAmAw64BC
 export const GifExpertApp = () => {
   const [categories, setCategories] = useState(['Attack On Titan', 'Ichigo 100%'])
-  const addAnime = (anime) => () => setCategories([...categories, anime]);
+  const addAnime = (anime) => setCategories([anime, ...categories]);
 
 
   return (
     <>
       <h1>Gif Expert App</h1>
       {/* <button onClick={() => setCategories([...categories, "Naruto"])}>Add Category</button> */}
-      <button onClick={addAnime('Naruto')}>Add Category</button>
+      <button onClick={() => addAnime('Naruto')}>Add Category</button>
       
       <ul>
         {categories.map(category => <li key={category}>{category}</li>)}
