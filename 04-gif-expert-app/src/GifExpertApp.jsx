ba320e535270d8ff4dd2d581bc64c375a4336eea
Fabztech <fabztech@outlook.com>
2025-04-13 23:56:27 -0500

    GifExpertApp - GifExpertApp component created, styles imported

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
 
-
-
+// bkUpWx7ebyORXlTH747bYSgrAmAw64BC
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Attack On Titan', 'Ichigo 100%'])
+  // console.log(categories);
+  
   return (
     <>
       <h1>Gif Expert App</h1>
+      <ul>
+        {categories.map(category => <li key={category}>{category}</li>)}
+      </ul>    
     </>
   );
 };
