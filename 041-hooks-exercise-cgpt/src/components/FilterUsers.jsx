f583dde1868c9f763eae362cbb09e006a13bc385
Fabztech <fabztech@outlook.com>
2025-04-27 22:11:01 -0500

    Excersice Chat GPT filter names DB Complete v2 isLoading implemented

--- 041-hooks-exercise-cgpt/src/components/FilterUsers.jsx
+++ 041-hooks-exercise-cgpt/src/components/FilterUsers.jsx
@@ -5,16 +5,19 @@ import { users } from "../assets/usersDB";
 export const FilterUsers = (props) => {
   console.log("Component Renderized...");
 
+  const [isLoading, setIsLoading] = useState(false);
   const [nameList, setNameList] = useState([]);
   const debounceTimer = useRef(null);
 
   const handleInputChange = (e) => {
+    setIsLoading(true);
     clearTimeout(debounceTimer.current);
     debounceTimer.current = setTimeout(() => {
       const newInputValue = e.target.value;
       const regex = newInputValue != "" && new RegExp(`^${newInputValue}`);
       const namesFound = users.filter((user) => user.name.match(regex));
       setNameList(namesFound);
+      setIsLoading(false);
     }, 3000);
   };
 
@@ -30,6 +33,7 @@ export const FilterUsers = (props) => {
         }}
       />
       <ul>
+        {isLoading && <h3>Loading...</h3>}
         {nameList.map((u) => (
           <li key={u.id}>{u.name}</li>
         ))}
